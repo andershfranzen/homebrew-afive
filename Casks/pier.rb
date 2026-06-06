edf065c0c327cf07589fe49c044260fbd528106c
ahftefcold <ahf@tefcold.com>
2026-06-06 08:25:54 +0200

    Add Pier cask

---
+++ Casks/pier.rb
@@ -0,0 +1,21 @@
+cask "pier" do
+  version "0.4.0"
+  sha256 "e2aabb9e7f63a7f3724752ffb58ec558f71105e4e125660de3bb9853f27e8ca9"
+
+  url "https://www.afive.dev/downloads/pier/Pier-#{version}.zip"
+  name "Pier"
+  desc "Menu-bar Docker fleet monitor over SSH"
+  homepage "https://www.afive.dev/pier"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Pier.app"
+
+  uninstall quit: "dk.afive.pier"
+
+  zap trash: [
+    "~/Library/Application Support/Pier",
+    "~/Library/Application Support/DockGlance",
+    "~/Library/Preferences/dk.afive.pier.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew tap for afive apps
+
+Install Pier:
+
+```sh
+brew tap andershfranzen/afive
+brew install --cask pier
+```
+
+Pier is a macOS menu-bar Docker fleet monitor over SSH.
